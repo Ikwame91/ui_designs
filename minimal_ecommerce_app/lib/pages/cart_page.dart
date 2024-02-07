d04e6f84f812c196ac0b11f3c04acc5ceace5c77
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-02-07 23:59:36 +0000

    minor

--- minimal_ecommerce_app/lib/pages/cart_page.dart
+++ minimal_ecommerce_app/lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_ecommerce_app/components/my_button.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -6,14 +7,35 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      foregroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: const Text(
-        'Cart Page',
-        style: TextStyle(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(
+          'Cart Page',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
       ),
-    ));
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.all(15),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Icon(Icons.newspaper_rounded),
+                    Text('Name'),
+                  ],
+                ),
+                MyButton(child: Icon(Icons.contactless_outlined))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
